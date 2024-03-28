e9e7cf50a05309cf275447fd8766129fa48a5b87
Karrar Qasim <Karawr1996@gmail.com>
2024-03-28 01:31:21 -0600

    Added names for data table

---
+++ src/components/DataTable/DataTable.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+function DataTable({ data }) {
+  return (
+    <div className="data-table">
+      <table>
+        <thead>
+          <tr>
+            <th>Band Name</th>
+            <th>Musician</th>
+            <th>Income</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item, index) => (
+            <tr key={index}>
+              <td>{item.bandName}</td>
+              <td>{item.name}</td>
+              <td>${item.income}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default DataTable;
