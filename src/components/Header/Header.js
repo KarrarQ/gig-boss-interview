6770b0ead2349cb388a54c86f2d64c1b6e801b4b
Karrar Qasim <Karawr1996@gmail.com>
2024-03-28 11:42:13 -0600

    Added some sylings to app

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,8 +7,6 @@ function Header() {
       <nav>
         <ul>
           <li><a href="/">Home</a></li>
-          <li><a href="/about">About</a></li>
-          <li><a href="/contact">Contact</a></li>
         </ul>
       </nav>
     </header>
