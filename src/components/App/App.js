db59e9e7eb678b26ea3654ba17b96d7b3028b855
Karrar Qasim <Karawr1996@gmail.com>
2024-03-28 14:42:01 -0600

    Added an onclick in exportation

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,23 +7,28 @@ import IncomeByMusician from '../IncomeByMusician/IncomeByMusician';
 import ExportDataButton from '../ExportDataButton/ExportDataButton';
 import DataTable from '../DataTable/DataTable';
 import SearchInput from '../Searchinput/Searchinput';
-import TotalIncome from '../TotalIncome/TotalIncome'; // Import TotalIncome component
+import TotalIncome from '../TotalIncome/TotalIncome';
 import bandsData from '../SampleData/income_data_2023_24.json'; 
 
 function App() {
   const [selectedBand, setSelectedBand] = useState(null); // State to store selected band
   const [searchQuery, setSearchQuery] = useState(''); // State to store search query
+  const [showDataTable, setShowDataTable] = useState(false); // State to toggle DataTable visibility
+
+  const handleExportData = () => {
+    setShowDataTable(true); // Show DataTable when Export Data button is clicked
+  };
 
   return (
     <main>
       <Header />
-      <TotalIncome selectedBand={selectedBand} bandsData={bandsData.bands} /> {/* Render TotalIncome component */}
+      <TotalIncome selectedBand={selectedBand} bandsData={bandsData.bands} />
       <DropDownMenu onSelectBand={setSelectedBand} bandsData={bandsData.bands} />
       <RenderArea selectedBand={selectedBand} bandsData={bandsData.bands} />
-      <IncomeByMusician selectedBand={selectedBand} bandsData={bandsData.bands} /> {/* Pass selectedBand to IncomeByMusician */}
-      <DataTable data={bandsData.bands.reduce((acc, band) => acc.concat(band.members.map(member => ({ bandName: band.band_name, ...member }))), [])} />
-      <ExportDataButton data={bandsData} />
+      <IncomeByMusician selectedBand={selectedBand} bandsData={bandsData.bands} />
+      <ExportDataButton onClick={handleExportData} />
       <SearchInput value={searchQuery} onChange={setSearchQuery} />
+      {showDataTable && <DataTable data={bandsData.bands.reduce((acc, band) => acc.concat(band.members.map(member => ({ bandName: band.band_name, ...member }))), [])} selectedBand={selectedBand} />}
     </main>
   );
 }
