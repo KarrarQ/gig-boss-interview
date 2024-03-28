861752e9fa8b9aeaa8dcc7763fe27f59aa4999e2
Karrar Qasim <Karawr1996@gmail.com>
2024-03-28 01:28:21 -0600

    Fixed the routing situation

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,21 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Header from '../Header/Header';
 import DropDownMenu from '../DropDownMenu/DropDownMenu';
 import RenderArea from '../RenderArea/RenderArea';
 import IncomeByMusician from '../IncomeByMusician/IncomeByMusician';
 import ExportDataButton from '../ExportDataButton/ExportDataButton';
-import bandsData from '../SampleData/income_data_2023_24.json.json'; 
-
+import DataTable from '../DataTable/DataTable';
+import SearchInput from '../Searchinput/Searchinput';
+import bandsData from '../SampleData/income_data_2023_24.json'; 
 function App() {
+  const [selectedBand, setSelectedBand] = useState(null); // State to store selected band
+  const [searchQuery, setSearchQuery] = useState(''); // State to store search query
+
   return (
     <main>
       <Header />
-      <DropDownMenu />
-      <RenderArea />
-      {/* Pass the bands data to the IncomeByMusician component */}
+      <DropDownMenu onSelectBand={setSelectedBand} bandsData={bandsData.bands} />
+      <RenderArea selectedBand={selectedBand} bandsData={bandsData.bands} />
       <IncomeByMusician bands={bandsData.bands} />
-      <ExportDataButton />
+      <DataTable data={bandsData.bands.reduce((acc, band) => acc.concat(band.members.map(member => ({ bandName: band.band_name, ...member }))), [])} />
+      <ExportDataButton data={bandsData} />
+      <SearchInput value={searchQuery} onChange={setSearchQuery} />
     </main>
   );
 }
