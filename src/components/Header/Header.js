717c37e32624ec4b0d64d8b665795b2654cca9d8
Karrar Qasim <Karawr1996@gmail.com>
2024-03-29 00:26:45 -0600

    Css fixes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,13 @@ function Header() {
   return (
     <header className="header">
       <nav>
-      <h1><a href="/">Gig Boss</a></h1>
+        <a href="/" className="gigboss-logo">
+          <img
+            src="//images.squarespace-cdn.com/content/v1/5b9952a7e7494023089aea6b/32beac78-6099-459e-9725-4f812358216a/Gig+Boss_Logo_Nov22_Logotype_White.png?format=1500w"
+            alt="Gig Boss logo"
+            style={{ height: "100%", width: "100%", objectFit: "contain" }}
+          />
+        </a>
       </nav>
     </header>
   );
